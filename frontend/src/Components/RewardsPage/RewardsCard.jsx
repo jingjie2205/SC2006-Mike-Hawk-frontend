fa7f619c78a51c7d386ccde850a7a0cf3420b98a
jingjie2205 <62003067+jingjie2205@users.noreply.github.com>
2024-10-15 23:12:42 +0800

    improved RewardsPage UI

--- frontend/src/Components/RewardsPage/RewardsCard.jsx
+++ frontend/src/Components/RewardsPage/RewardsCard.jsx
@@ -1,42 +1,109 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Box,
   Card,
   Text,
   Image,
-  CardHeader,
   CardBody,
-  CardFooter,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  useDisclosure,
+  Button,
+  useToast,
 } from "@chakra-ui/react";
 import "./RewardsCard.css";
 
-function RewardsCard({ image, name, points, amount }) {
+function RewardsCard({ image, name, points, amount, userPoints }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+  const toast = useToast();
+
+  const handleRedemption = () => {
+    if (userPoints >= points) {
+      toast({
+        title: "Redemption Successful!",
+        description: `You have redeemed ${name} voucher for ${points} points!`,
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      onClose();
+    } else {
+      toast({
+        title: "Insufficient Points",
+        description: "You do not have enough points to redeem this reward!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      onClose();
+    }
+  };
+
   return (
-    <Card as="button" width={"500px"} height={"200px"} className="rewards-card">
-      <CardBody
+    <>
+      <Card
+        as="button"
         width={"500px"}
         height={"200px"}
-        display="flex"
-        alignItems={"center"}
+        className="rewards-card"
+        onClick={onOpen}
       >
-        <Image
-          src={image}
-          width="100px"
-          height="90px"
-          rounded={3}
-          alignContent={"center"}
-        />
-        <Box
-          justifyContent="space-between"
+        <CardBody
           width={"500px"}
-          className="text-container"
+          height={"200px"}
+          display="flex"
+          alignItems={"center"}
         >
-          <Text>{name}</Text>
-          <Text>${amount} evoucher</Text>
-          <Text>{points} points</Text>
-        </Box>
-      </CardBody>
-    </Card>
+          <Image
+            src={image}
+            width="100px"
+            height="90px"
+            rounded={3}
+            alignContent={"center"}
+          />
+          <Box
+            justifyContent="space-between"
+            width={"500px"}
+            className="text-container"
+          >
+            <Text>{name}</Text>
+            <Text>${amount} evoucher</Text>
+            <Text>{points} points</Text>
+          </Box>
+        </CardBody>
+      </Card>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Confirm Redemption
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure you want to redeem this reward for {points} points?
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                No
+              </Button>
+              <Button colorScheme="red" onClick={handleRedemption} ml={3}>
+                Yes
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 }
 
--- frontend/src/Components/RewardsPage/RewardsPage.jsx
+++ frontend/src/Components/RewardsPage/RewardsPage.jsx
@@ -1,23 +1,61 @@
 import React from "react";
 import NavBar from "../../Common/NavBar";
 import RewardsCard from "./RewardsCard";
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Stat, StatLabel, StatNumber } from "@chakra-ui/react";
 
 function RewardsPage() {
-  // hard coded for testing
+  {
+    /* HARD CODED FOR TESTING */
+  }
   const vouchers = [
     { image: "../../public/macs.png", name: "Macs", points: 100, amount: 10 },
-    // { image: "../../public/macs.png", name: "Macs", points: 100, amount: 10 },
+    {
+      image: "../../public/fairprice.png",
+      name: "FairPice",
+      points: 150,
+      amount: 10,
+    },
   ];
   return (
     <div>
       <NavBar />
       <Box
+        display="flex"
+        alignItems={"center"}
+        paddingTop={"50px"}
+        justifyContent="space-between"
+        maxWidth="600px"
+        mx="auto"
+        bg="teal.500"
+      >
+        <Text>Rewards</Text>
+      </Box>
+      <Box
+        borderWidth="2px"
+        borderRadius="lg"
+        maxW="sm"
+        display="flex"
+        justifyContent="center" // Center horizontally
+        alignItems="center" // Center vertically
+        padding="10px"
+        mx="auto" // Center the box itself on the page
+        my="20px"
+      >
+        {/* USER's POINTS */}
+        <Stat>
+          <StatLabel>Total Points</StatLabel>
+          {/* HARD CODED VALUE FOR TESTING */}
+          <StatNumber>100</StatNumber>
+        </Stat>
+      </Box>
+      {/* VOUCHERS */}
+      <Box
         display={"flex"}
         flexDir={"column"}
         alignItems={"center"}
-        paddingTop={"50px"}
+        paddingTop={"20px"}
       >
+        {/* HARD CODED VALUE FOR TESTING */}
         {vouchers.map((reward, index) => (
           <RewardsCard
             key={index}
@@ -25,6 +63,7 @@ function RewardsPage() {
             name={reward.name}
             points={reward.points}
             amount={reward.amount}
+            userPoints={100}
           />
         ))}
       </Box>
