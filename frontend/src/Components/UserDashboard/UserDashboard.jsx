b358456d3b14b1e3511702932cb5af34fa9e95eb
KehJingXiang <126039222+KehJingXiang@users.noreply.github.com>
2024-10-18 06:31:32 +0000

    update usermyreport page

--- frontend/src/Components/UserDashboard/UserDashboard.jsx
+++ frontend/src/Components/UserDashboard/UserDashboard.jsx
@@ -1,92 +1,178 @@
 import React from "react";
 import NavBar from "../../Common/NavBar";
-import { Box, Button, Flex, Avatar, Text, Input, VStack, IconButton, Image, HStack} from "@chakra-ui/react";
-import { FaSearch, FaRegThumbsUp, FaCommentDots, FaShare } from 'react-icons/fa';
+import {
+  Box,
+  Button,
+  Flex,
+  Avatar,
+  Text,
+  Input,
+  VStack,
+  IconButton,
+  Image,
+  HStack,
+} from "@chakra-ui/react";
+import {
+  FaSearch,
+  FaRegThumbsUp,
+  FaCommentDots,
+  FaShare,
+} from "react-icons/fa";
 import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+
+// Sample report data
+const reports = [
+  {
+    id: 1,
+    authority: "Jurong Fire Station",
+    authorityimage: "src/Assets/FireStation.png",
+    description:
+      "We regret to inform our community that the fire alarm system at Jurong Point Mall is currently not functioning due to a technical issue. Our team is working urgently to restore it to full operation. In the meantime, please remain vigilant and report any emergencies directly to 995. Your safety is our top priority, and we will update you as soon as the issue is resolved.",
+    image: "src/Assets/FireAlarm.jpg",
+    timestamp: "10:45 AM",
+  },
+  {
+    id: 2,
+    authority: "Jurong Fire Station",
+    authorityimage: "src/Assets/FireStation.png",
+    title: "Fire Drill Scheduled",
+    description:
+      "A fire drill will take place on November 1st at 2 PM. Please ensure all procedures are followed.",
+    image: "src/Assets/FireDrill.png",
+    timestamp: "09:00 AM",
+  }
+];
 
 function UserDashboard() {
   return (
     <div>
       <NavBar />
 
-      <VStack 
-        bg="#06ADBF"
-        align="center"
-        mt="3%"
-      >
-        <Text fontWeight="1000" mt="3%" fontSize="200%" align="center" color="white" mb="0px">Welcome John</Text>
+      <VStack bg="#06ADBF" align="center" mt="3%">
+        <Text
+          fontWeight="1000"
+          mt="3%"
+          fontSize="200%"
+          align="center"
+          color="white"
+          mb="0px"
+        >
+          Welcome John
+        </Text>
         <HStack alignItems="center" mt="0px">
           <IconButton
             aria-label="Search"
             icon={<FaSearch />}
-            fontSize='300%'
+            fontSize="300%"
             color="white"
             background="#06ADBF"
-            p="10" />
-          <Input placeholder="Search Announcements" htmlSize="50%" height={50} borderRadius={25} border-color="black" />
+            p="10"
+          />
+          <Input
+            placeholder="Search Announcements"
+            htmlSize="50%"
+            height={50}
+            borderRadius={25}
+            border-color="black"
+          />
           <IconButton
             aria-label="Filter"
             icon={<HiOutlineAdjustmentsHorizontal />}
-            fontSize='400%'
+            fontSize="400%"
             color="white"
             background="#06ADBF"
-            p="10" />
+            p="10"
+          />
         </HStack>
       </VStack>
-      <VStack
-        bg="white"
-        align="center"
-      >
-        <Box
-          bg="#dddddd"
-          alignItems="center"
-          w="100%"
-          margin="3% 0px 3% 0px"
-          padding="3%">
-          <Image mt="1%" mr="3%" boxSize="15%" float='left' src="src/Assets/FireStation.png" alt="Fire Station" />
-          <Text align="left" fontWeight="1000" fontSize="150%" color="black" >Jurong Fire Station </Text>
-          <Text align="left" fontWeight="1000" fontSize="80%" color="black" >Posted at 10:45 am </Text>
-        </Box>
 
-        <Text margin="0% 5% 0% 5%" align="left" fontWeight="1000" fontSize="70%" color="black" >We regret to inform our community that the fire alarm system at Jurong Point Mall is currently not functioning due to a technical issue.
-          Our team is working urgently to restore it to full operation. In the meantime, please remain vigilant and report any emergencies directly to 995. Your safety is our top priority, and we will update you as soon as the issue is resolved."</Text>
-        <Image p="2%" boxSize='60%' src="src/Assets/FireAlarm.jpg" alt="Fire Alarm" />
-      </VStack>
-      
-      <HStack mt="1%">
-        <HStack ml="8%"> 
-          <IconButton
-            icon={<FaRegThumbsUp />}
-            aria-label="Like"
-            fontSize='200%'
-            color="grey"
-            background="white"
-          />
-          <Text fontWeight="bold" color="black">Like</Text>
-        </HStack>
+      {reports.map((report) => (
+        <>
+          <VStack bg="white">
+            <Box
+              bg="#dddddd"
+              w="100%"
+              margin="3% 0px 3% 0px"
+              padding="3%"
+            >
+              <Image
+                mt="1%"
+                mr="3%"
+                boxSize="15%"
+                float="left"
+                src={report.authorityimage}
+                alt={report.authority}
+              />
+              <Text
+                align="left"
+                fontWeight="1000"
+                fontSize="150%"
+                color="black"
+              >
+                {report.authority}
+              </Text>
+              <Text align="left" fontWeight="1000" fontSize="80%" color="black">
+                Posted at {report.timestamp}
+              </Text>
+            </Box>
 
-        <HStack ml="7%" mr="5%">
-          <IconButton
-            icon={<FaCommentDots />}
-            fontSize='200%'
-            color="grey"
-            background="white"
-          />
-          <Text fontWeight="bold" color="black">Comment</Text>
-        </HStack>
+            <Text
+              margin="0% 5% 0% 5%"
+              align="left"
+              fontWeight="1000"
+              fontSize="70%"
+              color="black"
+            >
+              {report.description}
+            </Text>
+            <Image
+              p="2%"
+              boxSize="60%"
+              src={report.image}
+              alt="Fire Alarm"
+            />
+          </VStack>
+          <HStack mt="1%" mb="2%">
+            <HStack ml="8%">
+              <IconButton
+                icon={<FaRegThumbsUp />}
+                aria-label="Like"
+                fontSize="200%"
+                color="grey"
+                background="white"
+              />
+              <Text fontWeight="bold" color="black">
+                Like
+              </Text>
+            </HStack>
 
+            <HStack ml="7%" mr="5%">
+              <IconButton
+                icon={<FaCommentDots />}
+                fontSize="200%"
+                color="grey"
+                background="white"
+              />
+              <Text fontWeight="bold" color="black">
+                Comment
+              </Text>
+            </HStack>
 
-        <HStack >
-          <IconButton
-            icon={<FaShare />}
-            aria-label="Share"
-            fontSize='200%'
-            color="grey"
-            background="white"
-          />
-          <Text fontWeight="bold" color="black">Share</Text>
-        </HStack>
-      </HStack>
+            <HStack>
+              <IconButton
+                icon={<FaShare />}
+                aria-label="Share"
+                fontSize="200%"
+                color="grey"
+                background="white"
+              />
+              <Text fontWeight="bold" color="black">
+                Share
+              </Text>
+            </HStack>
+          </HStack>
+        </>
+      ))}
     </div>
   );
 }
--- frontend/src/Components/UserMyReport/UserMyReport.jsx
+++ frontend/src/Components/UserMyReport/UserMyReport.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import NavBar from "../../Common/NavBar";
 import {
   Box,
-  Button,
-  Flex,
-  Avatar,
   Text,
   Input,
   VStack,
@@ -12,9 +9,31 @@ import {
   Image,
   HStack,
 } from "@chakra-ui/react";
-import {
-  FaSearch,
-} from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+
+// Sample report data 
+const reports = [
+  {
+    id: 1,
+    description: "Spoilt fire alarm at Jurong Point 5:30 PM, 3rd Sept 2024",
+    image: "src/Assets/FireAlarm.jpg",
+    isActive: true,
+  },
+  {
+    id: 2,
+    title: "Pothole at Tampines Street 81",
+    description: "Pothole at Tampines Street 81, 1:30pm 30th Aug 2024",
+    image: "src/Assets/pothole.jpg",
+    isActive: false,
+  },
+  {
+    id: 3,
+    title: "Aircon leak at Sengkang Interchange",
+    description: "Aircon leak at Sengkang Interchange, 5:00pm 12 May",
+    image: "src/Assets/AirconLeakage.webp",
+    isActive: false,
+  },
+];
 
 function UserMyReport() {
   return (
@@ -44,80 +63,103 @@ function UserMyReport() {
           mr="3%"
         />
       </HStack>
-      
-      <Text fontWeight="500" mt="3%" mr="5%" mb="3%" fontSize="100%" align="right" color="black">
+
+      <Text
+        fontWeight="500"
+        mt="3%"
+        mr="5%"
+        mb="3%"
+        fontSize="100%"
+        align="right"
+        color="black"
+      >
         Sorted by: Most recent
       </Text>
 
-      <Text fontWeight="500" mt="3%" ml="5%" mb="3%" fontSize="100%" align="left" color="black">
+      <Text
+        fontWeight="500"
+        mt="3%"
+        ml="5%"
+        mb="3%"
+        fontSize="100%"
+        align="left"
+        color="black"
+      >
         Active Reports
       </Text>
       <VStack bg="white" align="center">
-        <Box
-          bg="#dddddd"
-          alignItems="center"
-          w="80%"
-          margin="3% 0px 3% 0px"
-          padding="3%"
-        >
-          <Image
-            mt="1%"
-            mr="3%"
-            boxSize="30%"
-            float="left"
-            src="src/Assets/FireAlarm.jpg"
-            alt="Fire Alarm"
-          />
-          <Text mt="4%" align="left" fontWeight="500" fontSize="80%" color="black">
-          Spoilt fire alarm at Jurong
-          Point, 5:30pm 3rd Sept 2024          
-          </Text>
-        </Box>
+        {reports
+          .filter((report) => report.isActive)
+          .map((report) => (
+            <Box
+              bg="#dddddd"
+              alignItems="center"
+              w="80%"
+              margin="3% 0px 3% 0px"
+              padding="3%"
+            >
+              <Image
+                mt="1%"
+                mr="3%"
+                boxSize="30%"
+                float="left"
+                src={report.image}
+                alt="Fire Alarm"
+              />
+              <Text
+                mt="4%"
+                align="left"
+                fontWeight="500"
+                fontSize="80%"
+                color="black"
+              >
+                {report.description}
+              </Text>
+            </Box>
+          ))}
       </VStack>
 
-      <Text fontWeight="500" mt="3%" ml="5%" mb="3%" fontSize="100%" align="left" color="black">
+      <Text
+        fontWeight="500"
+        mt="3%"
+        ml="5%"
+        mb="3%"
+        fontSize="100%"
+        align="left"
+        color="black"
+      >
         Past Reports
       </Text>
       <VStack bg="white" align="center">
-        <Box
-          bg="#dddddd"
-          alignItems="center"
-          w="80%"
-          margin="3% 0px 3% 0px"
-          padding="3%"
-        >
-          <Image
-            mt="1%"
-            mr="3%"
-            boxSize="30%"
-            float="left"
-            src="src/Assets/pothole.jpg"
-            alt="Pothole"
-          />
-          <Text mt="4%" align="left" fontWeight="500" fontSize="80%" color="black">
-          Pothole at Tampines Street 81, 1:30pm 30th Aug 2024         
-          </Text>
-        </Box>
-        <Box
-          bg="#dddddd"
-          alignItems="center"
-          w="80%"
-          margin="3% 0px 3% 0px"
-          padding="3%"
-        >
-          <Image
-            mt="1%"
-            mr="3%"
-            boxSize="30%"
-            float="left"
-            src="src/Assets/AirconLeakage.webp"
-            alt="Aircon Leakage"
-          />
-          <Text mt="4%" align="left" fontWeight="500" fontSize="80%" color="black">
-          Aircon leak at Sengkang 
-          Interchange, 5:00pm 12 May          
-          </Text>
-        </Box>
+      {reports
+          .filter((report) => !report.isActive)
+          .map((report) => (
+            <Box
+              bg="#dddddd"
+              alignItems="center"
+              w="80%"
+              margin="3% 0px 3% 0px"
+              padding="3%"
+            >
+              <Image
+                mt="1%"
+                mr="3%"
+                boxSize="30%"
+                float="left"
+                src={report.image}
+                alt="Fire Alarm"
+              />
+              <Text
+                mt="4%"
+                align="left"
+                fontWeight="500"
+                fontSize="80%"
+                color="black"
+              >
+                {report.description}
+              </Text>
+            </Box>
+          ))}
       </VStack>
     </div>
   );
