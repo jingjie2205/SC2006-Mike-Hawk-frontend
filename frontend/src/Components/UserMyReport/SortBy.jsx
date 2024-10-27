9181cd95b86c047312c43716ec5f6af24eba6f9b
weixu <seetohweixu@gmail.com>
2024-10-27 15:55:45 +0800

    UserMyReport page sort, search features added

--- frontend/src/Components/UserMyReport/SortBy.jsx
+++ frontend/src/Components/UserMyReport/SortBy.jsx
@@ -1,30 +1,16 @@
-import { Select } from "@chakra-ui/react";
+import { Flex, Select } from "@chakra-ui/react";
 
 const SortBy = () => {
   return (
-    <SelectRoot collection={frameworks} size="sm" width="320px">
-      <SelectLabel>Select framework</SelectLabel>
-      <SelectTrigger>
-        <SelectValueText placeholder="Select movie" />
-      </SelectTrigger>
-      <SelectContent>
-        {frameworks.items.map((movie) => (
-          <SelectItem item={movie} key={movie.value}>
-            {movie.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </SelectRoot>
+    <Flex m="0 70% 0 10%">
+      <Select placeholder="Newest">
+        <option value="Oldest">Oldest</option>
+        <option value="Most Severe">Most Severe</option>
+        <option value="Least Severe">Least Severe</option>
+        <option value="Category">Category</option>
+      </Select>
+    </Flex>
   );
 };
-
-const frameworks = createListCollection({
-  items: [
-    { label: "React.js", value: "react" },
-    { label: "Vue.js", value: "vue" },
-    { label: "Angular", value: "angular" },
-    { label: "Svelte", value: "svelte" },
-  ],
-});
 
 export default SortBy;
