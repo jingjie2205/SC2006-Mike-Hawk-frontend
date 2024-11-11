9a6bd3ecff5edd160c6ee7dc2e88e13370b08613
maxwell <maxweliau123456@gmail.com>
2024-11-11 16:02:53 +0800

    fixed navigation to authority my reports page

--- frontend/src/Common/NavBar.jsx
+++ frontend/src/Common/NavBar.jsx
@@ -1,80 +1,92 @@
 import { useState } from "react";
 import {
-  Flex,
-  Box,
-  Link,
-  Button,
-  HStack,
-  Spacer,
-  Image,
-  Divider,
-  Tooltip,
+    Flex,
+    Box,
+    Link,
+    Button,
+    HStack,
+    Spacer,
+    Image,
+    Divider,
+    Tooltip,
 } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
 import {
-  IoHomeOutline,
-  IoGiftOutline,
-  IoFileTrayFullOutline,
+    IoHomeOutline,
+    IoGiftOutline,
+    IoFileTrayFullOutline,
 } from "react-icons/io5";
 import { GoReport } from "react-icons/go";
 
 function NavBar() {
-  const isAuthority = parseInt(localStorage.getItem("isAuthority"), 10); // Fetch isAuthority from local storage
+    const isAuthority = parseInt(localStorage.getItem("isAuthority"), 10); // Fetch isAuthority from local storage
 
-  return (
-    <Box
-      bg="gray.800"
-      width="100vw" // Limits the width of the navbar
-      mt={4} // Adds top margin for spacing
-      py={2} // Adds some padding for vertical spacing
-      shadow="md"
-    >
-      <Flex>
-        <HStack ml="5%" mr="5%" width="90%" justify="space-between">
-          <Image
-            src="/RQ.png"
-            alt="ReportQuest Logo"
-            height="50"
-            rounded="50%"
-          />
-          {/* Navigation Links */}
-          <Tooltip label="Dashboard">
-            <Link as={RouterLink} to="/userdashboard">
-              <IoHomeOutline size={30} color="white" />
-            </Link>
-          </Tooltip>
+    return (
+        <Box
+            bg="gray.800"
+            width="100vw" // Limits the width of the navbar
+            mt={4} // Adds top margin for spacing
+            py={2} // Adds some padding for vertical spacing
+            shadow="md"
+        >
+            <Flex>
+                <HStack ml="5%" mr="5%" width="90%" justify="space-between">
+                    <Image
+                        src="/RQ.png"
+                        alt="ReportQuest Logo"
+                        height="50"
+                        rounded="50%"
+                    />
+                    {/* Navigation Links */}
+                    <Tooltip label="Dashboard">
+                        <Link as={RouterLink} to="/userdashboard">
+                            <IoHomeOutline size={30} color="white" />
+                        </Link>
+                    </Tooltip>
 
-          {isAuthority === 0 && (
-            <>
-              <Tooltip label="Make Report">
-                <Link as={RouterLink} to="/makereport">
-                  <GoReport size={30} color="white" />
-                </Link>
-              </Tooltip>
+                    {isAuthority === 0 && (
+                        <>
+                            <Tooltip label="Make Report">
+                                <Link as={RouterLink} to="/makereport">
+                                    <GoReport size={30} color="white" />
+                                </Link>
+                            </Tooltip>
 
-              <Tooltip label="Rewards">
-                <Link as={RouterLink} to="/myrewards">
-                  <IoGiftOutline size={30} color="white" />
-                </Link>
-              </Tooltip>
-            </>
-          )}
+                            <Tooltip label="Rewards">
+                                <Link as={RouterLink} to="/myrewards">
+                                    <IoGiftOutline size={30} color="white" />
+                                </Link>
+                            </Tooltip>
+                        </>
+                    )}
 
-          <Tooltip label="My Reports">
-            <Link as={RouterLink} to="/myreports">
-              <IoFileTrayFullOutline size={30} color="white" />
-            </Link>
-          </Tooltip>
-          <Tooltip label="Profile">
-            <Link as={RouterLink} to="/profile">
-              <FaUserCircle size={30} color="white" />
-            </Link>
-          </Tooltip>
-        </HStack>
-      </Flex>
-    </Box>
-  );
+                    <Tooltip label="My Reports">
+                        {isAuthority ? (
+                            <Link as={RouterLink} to="/authoritymyreports">
+                                <IoFileTrayFullOutline
+                                    size={30}
+                                    color="white"
+                                />
+                            </Link>
+                        ) : (
+                            <Link as={RouterLink} to="/myreports">
+                                <IoFileTrayFullOutline
+                                    size={30}
+                                    color="white"
+                                />
+                            </Link>
+                        )}
+                    </Tooltip>
+                    <Tooltip label="Profile">
+                        <Link as={RouterLink} to="/profile">
+                            <FaUserCircle size={30} color="white" />
+                        </Link>
+                    </Tooltip>
+                </HStack>
+            </Flex>
+        </Box>
+    );
 }
 
 export default NavBar;
--- frontend/src/Components/AuthorityMyReport/AuthorityMyReport.jsx
+++ frontend/src/Components/AuthorityMyReport/AuthorityMyReport.jsx
@@ -9,205 +9,228 @@ import ReportItem from "../UserMyReport/ReportItem";
 import { useNavigate } from "react-router-dom";
 
 function AuthorityMyReport() {
-  const userId = localStorage.getItem("userId");
+    const userId = localStorage.getItem("userId");
 
-  const [searchQuery, setSearchQuery] = useState("");
-  const [reports, setReports] = useState([]);
-  const [filteredReports, setFilteredReports] = useState([]);
-  const [sortOption, setSortOption] = useState("Newest");
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
+    const [searchQuery, setSearchQuery] = useState("");
+    const [reports, setReports] = useState([]);
+    const [filteredReports, setFilteredReports] = useState([]);
+    const [sortOption, setSortOption] = useState("Newest");
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!userId) {
-      navigate("/login"); // Redirects to login if userId is missing
-      return;
-    }
-
-    const fetchUserReports = async () => {
-      try {
-        const response = await axios.get(
-          `${config.baseURL}/reports/reports/user/${userId}`
-        );
-        if (response.status === 200) {
-          setReports(response.data);
-          setFilteredReports(response.data);
+    useEffect(() => {
+        if (!userId) {
+            navigate("/login"); // Redirects to login if userId is missing
+            return;
         }
-      } catch (err) {
-        console.error("Error fetching user reports:", err);
-        setError("Failed to fetch reports. Please try again later.");
-      }
+
+        const fetchUserReports = async () => {
+            try {
+                const response = await axios.get(
+                    `${config.baseURL}/reports/reports/get_reports_by_authority_id/${userId}`
+                );
+                if (response.status === 200) {
+                    setReports(response.data);
+                    setFilteredReports(response.data);
+                }
+            } catch (err) {
+                console.error("Error fetching user reports:", err);
+                setError("Failed to fetch reports. Please try again later.");
+            }
+        };
+
+        fetchUserReports();
+    }, [userId]);
+
+    const handleSearch = () => {
+        setFilteredReports(
+            searchQuery.trim()
+                ? reports.filter((report) =>
+                      report.description
+                          .toLowerCase()
+                          .includes(searchQuery.toLowerCase())
+                  )
+                : reports
+        );
     };
 
-    fetchUserReports();
-  }, [userId]);
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            handleSearch();
+        }
+    };
 
-  const handleSearch = () => {
-    setFilteredReports(
-      searchQuery.trim()
-        ? reports.filter((report) =>
-            report.description.toLowerCase().includes(searchQuery.toLowerCase())
-          )
-        : reports
+    const handleSortChange = (event) => {
+        setSortOption(event.target.value);
+    };
+
+    const sortedReports = useMemo(() => {
+        const sortReports = [...filteredReports];
+        if (sortOption === "Most recent") {
+            return sortReports.sort(
+                (a, b) => new Date(b.Date) - new Date(a.Date)
+            );
+        }
+        if (sortOption === "Oldest") {
+            return sortReports.sort(
+                (a, b) => new Date(a.Date) - new Date(b.Date)
+            );
+        }
+        if (sortOption === "Most Severe") {
+            return sortReports.sort((a, b) => b.severity - a.severity);
+        }
+        if (sortOption === "Least Severe") {
+            return sortReports.sort((a, b) => a.severity - b.severity);
+        }
+        return sortReports;
+    }, [filteredReports, sortOption]);
+
+    const openReportDetails = (report) => {
+        navigate(`/report/${report.ReportID}`, { state: { report } });
+    };
+
+    return (
+        <div>
+            {error && (
+                <Box bg="red.100" p="4" mb="4" borderRadius="md">
+                    <Text color="red.700">{error}</Text>
+                </Box>
+            )}
+
+            <VStack bg="#06ADBF" align="center" mt="3%">
+                <Text
+                    fontWeight="1000"
+                    mt="3%"
+                    mb="3%"
+                    fontSize="200%"
+                    color="white"
+                >
+                    My Reports
+                </Text>
+            </VStack>
+
+            <HStack
+                alignItems="center"
+                mt="3%"
+                position={"sticky"}
+                m="3%"
+                p="10px"
+            >
+                <Input
+                    onKeyDown={handleKeyDown}
+                    placeholder="Search For Your Reports"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    height={45}
+                    borderRadius={25}
+                    backgroundColor="#D3D3D3"
+                    ml="3%"
+                />
+                <IconButton
+                    onClick={handleSearch}
+                    aria-label="Search"
+                    icon={<FaSearch />}
+                    fontSize="200%"
+                    background="white"
+                    color="grey"
+                    paddingRight="2%"
+                />
+            </HStack>
+
+            <HStack justifyContent={"space-between"} width="100%">
+                <SortBy
+                    sortOption={sortOption}
+                    onSortChange={handleSortChange}
+                />
+                <Text
+                    fontSize="110%"
+                    textDecor={"underline"}
+                    textAlign="right"
+                    mr="10%"
+                    onClick={() => {
+                        setSearchQuery("");
+                        setFilteredReports(reports);
+                    }}
+                    cursor="pointer"
+                >
+                    Show all reports
+                </Text>
+            </HStack>
+
+            <Text
+                fontWeight="500"
+                mb="2%"
+                fontSize="250%"
+                align="center"
+                color="black"
+            >
+                Active Reports
+            </Text>
+
+            <VStack bg="white" align="center">
+                {sortedReports.filter(
+                    (report) =>
+                        report.status === "In Progress" ||
+                        report.status === "Pending"
+                ).length === 0 ? (
+                    <Box bg="#dddddd" w="80%" margin="3% 0" padding="3%">
+                        <Text fontWeight={"400"} fontSize={"120%"}>
+                            No reports found
+                        </Text>
+                    </Box>
+                ) : (
+                    sortedReports
+                        .filter(
+                            (report) =>
+                                report.status === "In Progress" ||
+                                report.status === "Pending"
+                        )
+                        .map((report) => (
+                            <ReportItem
+                                key={report.report_id}
+                                report={report}
+                                onClick={() => openReportDetails(report)}
+                            />
+                        ))
+                )}
+            </VStack>
+
+            <Text
+                fontWeight="500"
+                mt="1%"
+                mb="2%"
+                fontSize="250%"
+                align="center"
+                color="black"
+            >
+                Past Reports
+            </Text>
+
+            <VStack bg="white" align="center">
+                {sortedReports.filter((report) => report.status === "Resolved")
+                    .length === 0 ? (
+                    <Box bg="#dddddd" w="80%" margin="3% 0" padding="3%">
+                        <Text fontWeight={"400"} fontSize={"120%"}>
+                            No reports found
+                        </Text>
+                    </Box>
+                ) : (
+                    sortedReports
+                        .filter((report) => report.status === "Resolved")
+                        .map((report) => (
+                            <ReportItem
+                                key={report.report_id}
+                                report={report}
+                                onClick={() => openReportDetails(report)}
+                            />
+                        ))
+                )}
+            </VStack>
+            <Box position="fixed" bottom="0" width="100%" overflow="hidden">
+                <NavBar />
+            </Box>
+        </div>
     );
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      handleSearch();
-    }
-  };
-
-  const handleSortChange = (event) => {
-    setSortOption(event.target.value);
-  };
-
-  const sortedReports = useMemo(() => {
-    const sortReports = [...filteredReports];
-    if (sortOption === "Most recent") {
-      return sortReports.sort((a, b) => new Date(b.Date) - new Date(a.Date));
-    }
-    if (sortOption === "Oldest") {
-      return sortReports.sort((a, b) => new Date(a.Date) - new Date(b.Date));
-    }
-    if (sortOption === "Most Severe") {
-      return sortReports.sort((a, b) => b.severity - a.severity);
-    }
-    if (sortOption === "Least Severe") {
-      return sortReports.sort((a, b) => a.severity - b.severity);
-    }
-    return sortReports;
-  }, [filteredReports, sortOption]);
-
-  const openReportDetails = (report) => {
-    navigate(`/report/${report.ReportID}`, { state: { report } });
-  };
-
-  return (
-    <div>
-      {error && (
-        <Box bg="red.100" p="4" mb="4" borderRadius="md">
-          <Text color="red.700">{error}</Text>
-        </Box>
-      )}
-
-      <VStack bg="#06ADBF" align="center" mt="3%">
-        <Text fontWeight="1000" mt="3%" mb="3%" fontSize="200%" color="white">
-          My Reports
-        </Text>
-      </VStack>
-
-      <HStack alignItems="center" mt="3%" position={"sticky"} m="3%" p="10px">
-        <Input
-          onKeyDown={handleKeyDown}
-          placeholder="Search For Your Reports"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          height={45}
-          borderRadius={25}
-          backgroundColor="#D3D3D3"
-          ml="3%"
-        />
-        <IconButton
-          onClick={handleSearch}
-          aria-label="Search"
-          icon={<FaSearch />}
-          fontSize="200%"
-          background="white"
-          color="grey"
-          paddingRight="2%"
-        />
-      </HStack>
-
-      <HStack justifyContent={"space-between"} width="100%">
-        <SortBy sortOption={sortOption} onSortChange={handleSortChange} />
-        <Text
-          fontSize="110%"
-          textDecor={"underline"}
-          textAlign="right"
-          mr="10%"
-          onClick={() => {
-            setSearchQuery("");
-            setFilteredReports(reports);
-          }}
-          cursor="pointer"
-        >
-          Show all reports
-        </Text>
-      </HStack>
-
-      <Text
-        fontWeight="500"
-        mb="2%"
-        fontSize="250%"
-        align="center"
-        color="black"
-      >
-        Active Reports
-      </Text>
-
-      <VStack bg="white" align="center">
-        {sortedReports.filter(
-          (report) =>
-            report.status === "In Progress" || report.status === "Pending"
-        ).length === 0 ? (
-          <Box bg="#dddddd" w="80%" margin="3% 0" padding="3%">
-            <Text fontWeight={"400"} fontSize={"120%"}>
-              No reports found
-            </Text>
-          </Box>
-        ) : (
-          sortedReports
-            .filter(
-              (report) =>
-                report.status === "In Progress" || report.status === "Pending"
-            )
-            .map((report) => (
-              <ReportItem
-                key={report.report_id}
-                report={report}
-                onClick={() => openReportDetails(report)}
-              />
-            ))
-        )}
-      </VStack>
-
-      <Text
-        fontWeight="500"
-        mt="1%"
-        mb="2%"
-        fontSize="250%"
-        align="center"
-        color="black"
-      >
-        Past Reports
-      </Text>
-
-      <VStack bg="white" align="center">
-        {sortedReports.filter((report) => report.status === "Resolved")
-          .length === 0 ? (
-          <Box bg="#dddddd" w="80%" margin="3% 0" padding="3%">
-            <Text fontWeight={"400"} fontSize={"120%"}>
-              No reports found
-            </Text>
-          </Box>
-        ) : (
-          sortedReports
-            .filter((report) => report.status === "Resolved")
-            .map((report) => (
-              <ReportItem
-                key={report.report_id}
-                report={report}
-                onClick={() => openReportDetails(report)}
-              />
-            ))
-        )}
-      </VStack>
-      <Box position="fixed" bottom="0" width="100%" overflow="hidden">
-        <NavBar />
-      </Box>
-    </div>
-  );
 }
 
 export default AuthorityMyReport;
